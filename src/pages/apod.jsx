2dad3b874dc00d5d3aef0ddcfc2536e29e3eee07
Anshuman Gupta <anshuman844@gmail.com>
2021-03-15 14:12:42 +0530

    chore: Include styles from page.module.scss

--- src/pages/apod.jsx
+++ src/pages/apod.jsx
@@ -3,13 +3,14 @@ import ApodLoader from '../components/loader/apod';
 import ErrorBanner from '../components/error';
 import { useAxios } from '../adapters';
 import { api } from '../app';
+import pages from '../styles/pages/page.module.scss';
 import styles from '../styles/pages/apod.module.scss';
 
 const ApodPage = props => {
   const [response, error, waiting] = useAxios(`${api}planetary/apod?api_key=${process.env.REACT_APP_API_KEY}`)
   return (
-    <main className={styles.apod}>
-      <h1 className={styles.apod__page_title}>
+    <main className={`${pages.page} ${styles.apod}`}>
+      <h1 className={pages.page__title}>
         Astronomy Picture of the Day
       </h1>
       { waiting && <ApodLoader /> }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,13 @@
 import Card from '../components/card';
 import { Link } from 'react-router-dom';
 import { sections } from '../app';
+import pages from '../styles/pages/page.module.scss'; 
 import styles from '../styles/pages/home.module.scss';
 
 const Home = props => {
   return (
-    <main className={styles.home}>
-      <h1 className={styles.home__title}>Explore</h1>
+    <main className={`${pages.page} ${styles.home}`}>
+      <h1 className={pages.page__title}>Explore</h1>
       {
         sections.map(({title, image, link, desc}, idx) => (
           <Link to={link} key={idx} className={styles.home__card}>

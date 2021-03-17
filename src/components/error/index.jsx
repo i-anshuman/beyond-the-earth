c0b2e671ebed2dcad77b9648d598f11ba56d3ec8
Anshuman Gupta <anshuman844@gmail.com>
2021-03-17 23:35:37 +0530

    feat: Add message props

--- src/components/error/index.jsx
+++ src/components/error/index.jsx
@@ -1,18 +1,32 @@
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import styles from '../../styles/components/error.module.scss';
 
-const ErrorBanner = props => {
+const ErrorBanner = ({message, linkToHome}) => {
   return (
     <div className={styles.error}>
       <h1 className={styles.error__title}>
-        Ops... Something went wrong.
+        {message}
       </h1>
       <h3>Try again later</h3>
-      <Link to="/" className={styles.error__back}>
-        Back to Home
-      </Link>
+      {
+        linkToHome &&
+        <Link to="/" className={styles.error__back}>
+          Back to Home
+        </Link>
+      }
     </div>
   );
+};
+
+ErrorBanner.defaultProps = {
+  message: "Ops... Something went wrong.",
+  linkToHome: true
+};
+
+ErrorBanner.propTypes = {
+  message: PropTypes.string,
+  linkToHome: PropTypes.bool
 };
 
 export default ErrorBanner;
